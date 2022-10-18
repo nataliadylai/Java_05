60d2d98dd8b06b2badb74919989f1fc0677de6e0
nataliadylai <dylainatalia@gmail.com>
2022-10-18 12:26:24 -0500

    added file FizzBuzz

---
+++ src/main/java/Utils.java
@@ -0,0 +1,13 @@
+public class Utils {
+
+
+    public String printLn() {
+        return ("***********************");
+
+
+        int tcNumber =1;
+        public String printTCNumber() {
+            return ("TC_" + tcNumber++ + "\n");
+        }
+    }
+}
